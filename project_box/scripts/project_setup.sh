303823bdaff6d01dd0c2e8e3d4e743c5a26a2ce6
Jurgen Verhasselt <sjugge@heretiksambrosia.net>
2013-09-08 16:24:22 +0200

    project setup: drupal settings

--- project_box/scripts/project_setup.sh
+++ project_box/scripts/project_setup.sh
@@ -34,6 +34,46 @@ LATESTDB=`ls -t /home/vagrant/db | grep .sql | head -1`
 mysql -u VAGRANT -pVAGRANT VAGRANT < /home/vagrant/db/$LATESTDB
 
 
+##### SETTINGS #####
+
+D6SETTINGS='/home/vagrant/scripts/resources/drupal_6.settings.php'
+D7SETTINGS='/home/vagrant/scripts/resources/drupal_7.settings.php'
+SETTINGSPATH='/var/www/sites/default/settings.php'
+
+if [ -e "$SETTINGSPATH" ]
+then
+  
+  echo "[vagrant project provisioning] Settings file already found. Moving current settings file to 'settings.php.backup'..."
+  /bin/mv $SETTINGSPATH $SETTINGSPATH.backup
+
+fi
+
+echo "[vagrant project provisioning] Creating settings file..."
+# Check which Drupal version is present, so that the correct settings file us used..."
+DRVE=`drush -r /var/www status | grep 'Drupal version' | sed 's/.*\([0-9]\)\..*$/\1/'`
+if [ "$DRVE" = 7 ]
+then 
+
+  echo "[vagrant project provisioning] Found a Drupal 7 website, copying settings file ..."
+    mv $D7SETTINGS $SETTINGSPATH
+
+elif [ "$DRVE" = 6 ]
+then 
+
+  echo "[vagrant project provisioning] Found a Drupal 6 website, copying settings file ..."
+    mv $D6SETTINGS $SETTINGSPATH
+
+fi
+
+
+##### CACHE & CRON #####
+
+echo "[vagrant project provisioning] Clearing cache & running cron..."
+drush cc drush
+drush -r /var/www cc all
+drush -r /var/www cron
+
+
 ##### PROJECT PROVISION CHECK #####
 
 # Create .project_provision_check for the script to check on during a next vargant up.
